88b2b61e8bc87741505ae6055252fa0a44e02c78
Pooya-Khani <pooyakhorshidkhani@gmail.com>
2025-09-18 15:16:16 +0330

    bug fix

--- src/components/models/hero_models/HeroExperience.jsx
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -9,7 +9,7 @@ import { Suspense } from "react";
 
 const HeroExperience = () => {
 	const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-	const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
+	// const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
 	return (
 		<Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
@@ -18,9 +18,9 @@ const HeroExperience = () => {
 			{/* Configure OrbitControls to disable panning and control zoom based on device type */}
 			<OrbitControls
 				enablePan={false} // Prevents panning of the scene
-				enableZoom={!isTablet} // Disables zoom on tablets
-				maxDistance={20} // Maximum distance for zooming out
-				minDistance={5} // Minimum distance for zooming in
+				enableZoom={false} // Disables zoom on tablets
+				// maxDistance={20} // Maximum distance for zooming out
+				// minDistance={5} // Minimum distance for zooming in
 				minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
 				maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
 			/>
